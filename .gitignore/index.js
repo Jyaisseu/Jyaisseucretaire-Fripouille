a5ad0572178349dc0951b5c2250c2854e2e4f726
Jyaisseu <63356944+Jyaisseu@users.noreply.github.com>
2020-05-04 16:15:58 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,51 @@
+const Discord = require("discord.js");
+const mongoose = require("mongoose");
+const Eco = require("./node_modules/modules/economie");
+const client = new Discord.Client();
+var prefix = "!";
+
+mongoose.connect("mongodb+srv://Jyaisseu:er4007rp4011@jyaisseucretaire-jojo-fripouille-zlrys.mongodb.net/Jyaisseucrétaire-Jojo-Fripouille?retryWrites=true&w=majority", {useUnifiedTopology: true, useNewUrlParser: true }).then(() => console.log("MongoDB connecté"))
+
+client.login("Njk2MzU5NTA0NDk2OTUxMzc3.XpsOrA.3SOdwlDA2l2aADs4y8wNSNxanis")
+
+client.on("message", message =>{
+    if(!message.guild) return
+    if(message.content === prefix + "coucou")
+        message.channel.send("Et bah coucou à toi aussi, " + message.author.username + " !") 
+});
+
+client.on("guildMemberAdd", user =>{
+    const channel = user.guild.channels.cache.get("630431095065935884")
+    channel.send("Bienvenue <@!" + user.id + "> sur le meilleur serveur francophone que la Terre ait connu!")
+});
+
+client.on("guildMemberRemove", user =>{
+    const channel = user.guild.channels.cache.get("630431095065935884")
+    channel.send("Sniff... <@!" + user.id + "> a quitté le serveur! On était si bien pourtant!")
+});
+
+client.on("message", message =>{
+Eco.findOne({
+    User_ID: message.author.id
+}, (err, economie) => {
+    if(err) console.log(err)
+    if(!economie){
+        var compte = new Eco({
+            User_ID: message.author.id,
+            pseudo: message.author.username,
+            xp: 10,
+            level: 1
+        })
+        compte.save()
+    }else{
+        economie.xp = economie.xp +1
+        var main_level = economie.level
+        var next_level = economie.level * 20
+        if(next_level <= economie.xp){
+            economie.level = main_level +1
+            message.channel.send(`GG ${message.author} tu viens de passer niveau ${main_level +1} ! Tu deviens de plus en plus BG !`)
+        }
+        economie.save()
+    }
+})
+})
