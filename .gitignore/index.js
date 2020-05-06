bc66a32a9f5d021840b5841e78d2c0bf3786ad15
Jyaisseu <63356944+Jyaisseu@users.noreply.github.com>
2020-05-06 14:46:39 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,6 +4,8 @@ const Eco = require("../app/modules/economie.js");
 const client = new Discord.Client();
 var prefix = "!";
 
+process.on("--unhandled-rejections=strict")=strict
+
 mongoose.connect("mongodb+srv://Jyaisseu:er4007rp4011@jyaisseuctetaire-fripouille-zlrys.mongodb.net/Jyaisseucrétaire-Jojo-Fripouille?retryWrites=true&w=majority", {useUnifiedTopology: true, useNewUrlParser: true })
 
 client.login("Njk3MzgzOTUxODU3MzUyNzE1.XrEK3A.QUZvWDkm42ow1FpO_lhA5_NfGMw")
