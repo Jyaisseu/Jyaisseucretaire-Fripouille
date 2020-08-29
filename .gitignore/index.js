7fad0a3702b70aebd92a766cc2bb6fef6ccee24a
Jyaisseu <63356944+Jyaisseu@users.noreply.github.com>
2020-08-29 14:56:39 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -74,7 +74,50 @@ Eco.findOne({
             economie.xp = 0             
             
             client.channels.get("715144143306883092").send(`GG ${message.author} tu viens de passer niveau ${main_level +1} ! Tu deviens de plus en plus BG !`);
-            };
+
+            if(economie.level === 2){
+                const member = message.author.id
+                message.member.addRole('749245805281673248')
+                const débutant = message.member.guild.roles.get("749245805281673248")
+                client.channels.get("630431095065935884").send(`Félicitations à ${message.author} pour avoir obtenu le grade ${débutant} ! Bienvenue dans la famille !`)
+            }else{
+            if(economie.level === 10){
+                const member = message.author.id
+                message.member.removeRole("749245805281673248")
+                message.member.addRole("749245861560713246")
+                const actif = message.member.guild.roles.get("749245861560713246")
+                client.channels.get("630431095065935884").send(`Félicitations à ${message.author} pour avoir obtenu le grade ${actif} ! Tu es un(e) de nos membres les plus fidèles !`)
+            }else{
+            if(economie.level === 20){
+                const member = message.author.id
+                message.member.removeRole("749245861560713246")
+                message.member.addRole("749245970088329278")
+                const hyperactif = message.member.guild.roles.get("749245970088329278")
+                client.channels.get("630431095065935884").send(`Félicitations à ${message.author} pour avoir obtenu le grade ${hyperactif} ! Il ne faudrait pas que tu oublies ta vie sociale quand même !`)
+            }else{
+            if(economie.level === 50){
+                const member = message.author.id
+                message.member.removeRole("749245970088329278")
+                message.member.addRole("749246025197158411")
+                const hermite = message.member.guild.roles.get("749246025197158411")
+                client.channels.get("630431095065935884").send(`Mes amis, nous avons un nouveau ${hermite}... ${message.author} a décidé de passer sa vie sur le serveur !`)
+            }else{
+            if(economie.level === 80){
+                const member = message.author.id
+                message.member.removeRole("749246025197158411")
+                message.member.addRole("749246081942159370")
+                const empereur = message.member.guild.roles.get("749246081942159370")
+                client.channels.get("630431095065935884").send(`L'heure est grave... ${message.author} est sacré empereur de ce serveur ! Vive l'empereur !`)
+            }else{
+            if(economie.level === 100){
+                const member = message.author.id
+                message.member.removeRole("749246081942159370")
+                message.member.addRole("749246155053072405")
+                const dieu = message.member.guild.roles.get("749246155053072405")
+                client.channels.get("630431095065935884").send(`Sa venue nous était annoncée... ${message.author} s'est éveillé en temps que ${dieu} de ce serveur ! Que gloire et fierté lui soit alloué !`)
+            }
+            }}}}}                    
+        };
         economie.save()        
     }
 })
