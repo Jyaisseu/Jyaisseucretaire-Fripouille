77eb2cbb657912a5f1029e483b61e621f3a101d2
Jyaisseu <63356944+Jyaisseu@users.noreply.github.com>
2020-11-11 16:25:06 +0100

    Update stop.js

--- .gitignore/commandes/stop.js
+++ .gitignore/commandes/stop.js
@@ -2,13 +2,13 @@ const Discord = require("discord.js");
 const ytdl = require('ytdl-core');
 
 module.exports.run = async (client, message) => {
-    if(!message.member.voiceChannel)
+    if(!message.member.voice.channel)
         return message.channel.send("Connectez-vous au salon vocal Musique !")
-    if(!message.guild.me.voiceChannel)
+    if(!message.guild.me.voice.channel)
         return message.channel.send("Le bot ne joue pas de musiques actuellement !")
-    if(message.guild.me.voiceChannelID !== message.member.voiceChannelID)
+    if(message.guild.me.voice.channel.id !== message.member.voice.channel.id)
         return message.channel.send("Vous devez être connecté dans le même salon vocal que le bot !")
-    message.guild.me.voiceChannel.leave();
+    message.guild.me.voice.channel.leave();
     message.channel.send("La musique a correctement été arrêtée !");
 }
 
