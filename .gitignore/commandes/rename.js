5f27e688db2392240b107c32b89ab5b3483e4817
Jocelyn Saint-Ellier <63356944+Jyaisseu@users.noreply.github.com>
2021-04-25 17:16:47 +0200

    Update rename.js

--- .gitignore/commandes/rename.js
+++ .gitignore/commandes/rename.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const mongoose = require("mongoose");
-const Eco = require("C:/Users/JOCELYN/Documents/Discord/Jyaisseucrétaire/jojo_la_fripouille/modules/economie");
+const Eco = require("/app/modules/economie.js");
 
 module.exports.run = async (client, message) => {
     Eco.findOne({
