0ec082906cfb1cce7b09c580082686f739451d82
Jocelyn Saint-Ellier <63356944+Jyaisseu@users.noreply.github.com>
2021-04-25 17:14:49 +0200

    Update help.js

--- .gitignore/commandes/help.js
+++ .gitignore/commandes/help.js
@@ -1,8 +1,20 @@
 const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 
 module.exports.run = async (client, message) => {
-    message.channel.send("Voici la liste des commandes du bot:\n!coucou : pour que le bot puisse te rappeler qu'il sera toujours là pour toi!\n!level : affiche ton niveau de BG actuel, ainsi que ton nombre de point BG et ce qu'il te manque pour passer niveau supérieur.\n!play [URL Youtube] : Joue une musique de Youtube dans le salon vocal où tu te trouve.\n!stop : Déconnecte le bot du salon vocal où tu te trouve.\n!top5: Affiche les 5 membres les plus BG du serveur.\n!help : Affiche la liste des commands disponibles.")
+    const embed = new MessageEmbed()
+    .setTitle("Liste des commandes du bot Jyaisseucrétaire")
+    .setDescription("Pour connaître dans les moindres détails votre bot préféré !")
+    .addField("!coucou", "Pour que le bot puisse te rappeler qu'il sera toujours là pour toi !")
+    .addField("!level", "Affiche ton niveau de BG actuel, ainsi que ton nombre de points BG et ce qu'il te manque pour passer niveau supérieur.")
+    .addField("!top5", "Affiche les 5 membres les plus BG du serveur.")
+    .addField("!give [mentionner la personne] [nombre de point BG]", "Permet de donner une partie de ses points BG à une autre personne du serveur.")
+    .addField("!play [URL YouTube]", "Joue une musique dans le salon vocal où tu te trouves.")
+    .addField("!stop", "Déconnecte le bot du salon vocal où tu te trouves.")
+    .addField("!help", "Affiche la liste des commandes disponibles")
+    message.channel.send(embed);
 }
+
 module.exports.help = {
     name: "help"
 }
