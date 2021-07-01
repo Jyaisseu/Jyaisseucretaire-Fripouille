87d28eed638c2b5e147366327cf24d68179ae8c5
Jocelyn Saint-Ellier <63356944+Jyaisseu@users.noreply.github.com>
2021-07-01 12:57:29 +0200

    Update remove.js

--- .gitignore/commandes/remove.js
+++ .gitignore/commandes/remove.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose")
 const client = new Discord.Client();
 const Eco = require('/app/modules/economie.js')
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (client, message, args) => {
     if(message.author.bot) return;
-    var mentionned = message.mentions.users.first()
+    var mentionned = message.mentions.members.first()
     if(!message.guild.member(message.author).permissions.has("ADMINISTRATOR")) return message.reply("Désolé, vous n'avez pas la permission d'exécuter cette commande.")
     if(message.mentions.users.size === 0) return message.channel.send("Vous n'avez pas mentionné d'utilisateur !")
     if(mentionned.id === message.author.id) return message.channel.send("Désolé, vous ne pouvez pas vous donnez à vous-même des points BG!")
@@ -16,7 +16,7 @@ module.exports.run = async (bot, message, args) => {
             var difference = args.slice(1)
             var main_level = economie.level
             var perte = parseInt(difference)
-            if(perte > main_level *10 +economie.level) return message.channel.send("L'utilisateur ne possède pas la quantité de points BG que vous voulez enlever !")
+            if(perte > economie.total) return message.channel.send("L'utilisateur ne possède pas la quantité de points BG que vous voulez enlever !")
             if(perte > economie.xp){
                 var surplus = (perte - economie.xp)
                 do {
@@ -26,6 +26,63 @@ module.exports.run = async (bot, message, args) => {
                     var négatif = 0 -economie.xp
                 }while (négatif >0)
                 message.client.channels.cache.get("715144143306883092").send(`${mentionned} est redescendu niveau ${economie.level} !`)
+
+                if(economie.level <2) {
+                    const member = mentionned
+                    member.roles.remove("749246155053072405")
+                    member.roles.remove("749246081942159370")
+                    member.roles.remove("749246025197158411")
+                    member.roles.remove("749245970088329278")
+                    member.roles.remove("749245861560713246")
+                    member.roles.remove("749245805281673248")
+                    client.channels.cache.get("630431095065935884").send(`${mentionned} a perdu tous ses grades !`)
+                }else{
+                    if((economie.level >= 2) && (economie.level<10) && (!mentionned.roles.cache.has("749245805281673248"))) {
+                            const member = mentionned
+                            member.roles.remove("749246155053072405")
+                            member.roles.remove("749246081942159370")
+                            member.roles.remove("749246025197158411")
+                            member.roles.remove("749245970088329278")
+                            member.roles.remove("749245861560713246")
+                            member.roles.add('749245805281673248')
+                            const débutant = message.member.guild.roles.cache.get("749245805281673248")
+                            client.channels.cache.get("630431095065935884").send(`${mentionned} redescend au grade ${débutant}.`)
+                }else{
+                    if((economie.level >= 10) && (economie.level < 20) && (!mentionned.roles.cache.has("749245861560713246"))){
+                            const member = mentionned
+                            member.roles.remove("749246155053072405")
+                            member.roles.remove("749246081942159370")
+                            member.roles.remove("749246025197158411")
+                            member.roles.remove("749245970088329278")
+                            member.roles.add("749245861560713246")
+                            const actif = message.member.guild.roles.cache.get("749245861560713246")
+                            client.channels.cache.get("630431095065935884").send(`${mentionned} redescend au grade ${actif}.`)
+                }else{
+                    if((economie.level >= 20) && (economie.level < 50) && (!mentionned.roles.cache.has("749245970088329278"))){
+                            const member = mentionned
+                            member.roles.remove("749246155053072405")
+                            member.roles.remove("749246081942159370")
+                            member.roles.remove("749246025197158411")
+                            member.roles.add("749245970088329278")
+                            const hyperactif = message.member.guild.roles.cache.get("749245970088329278")
+                            client.channels.cache.get("630431095065935884").send(`${mentionned} redescend au grade ${hyperactif}.`)
+                }else{
+                    if((economie.level >= 50) && (economie.level < 80) && (!mentionned.roles.cache.has("749246025197158411"))){
+                            const member = mentionned
+                            member.roles.remove("749246155053072405")
+                            member.roles.remove("749246081942159370")
+                            member.roles.add("749246025197158411")
+                            const hermite = message.member.guild.roles.cache.get("749246025197158411")
+                            client.channels.cache.get("630431095065935884").send(`${mentionned} redescend au grade ${hermite}.`)
+                }else{
+                    if((economie.level >= 80) && (economie.level < 100) && (!mentionned.roles.cache.has("749246081942159370"))){
+                        const member = mentionned
+                        member.roles.remove("749246155053072405")
+                        member.roles.add("749246081942159370")
+                        const empereur = message.member.guild.roles.cache.get("749246081942159370")
+                        client.channels.cache.get("630431095065935884").send(`${mentionned} redescend au grade ${empereur}.`)
+                    }
+                }}}}}
             }else{
                 economie.xp = (economie.xp - perte)
             }
