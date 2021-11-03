e59cffa4678da598864605d88ee80ffa88ebccd4
Jocelyn Saint-Ellier <63356944+Jyaisseu@users.noreply.github.com>
2021-11-03 16:16:38 +0100

    Update help.js

--- .gitignore/commandes/help.js
+++ .gitignore/commandes/help.js
@@ -6,7 +6,7 @@ module.exports.run = async (client, message) => {
     .setTitle("Liste des commandes du bot Jyaisseucrétaire")
     .setDescription("Pour connaître dans les moindres détails votre bot préféré !")
     .addField("!coucou", "Pour que le bot puisse te rappeler qu'il sera toujours là pour toi !")
-    .addField("!level", "Affiche ton niveau de BG actuel, ainsi que ton nombre de points BG et ce qu'il te manque pour passer niveau supérieur.")
+    .addField("!level", "Affiche ton niveau de BG actuel, ainsi que ton nombre de points BG et ce qu'il te manque pour passer au niveau supérieur.")
     .addField("!top5", "Affiche les 5 membres les plus BG du serveur.")
     .addField("!give [mentionner la personne] [nombre de point BG]", "Permet de donner une partie de ses points BG à une autre personne du serveur.")
     .addField("!play [URL YouTube]", "Joue une musique dans le salon vocal où tu te trouves.")
