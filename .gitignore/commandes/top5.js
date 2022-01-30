9f644f9a86577f86eaba363a9cb75d64cf1ea159
Jocelyn Saint-Ellier <63356944+Jyaisseu@users.noreply.github.com>
2022-01-30 16:30:11 +0100

    Update top5.js

--- .gitignore/commandes/top5.js
+++ .gitignore/commandes/top5.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const { MessageEmbed } = require("discord.js");
 const mongoose = require("mongoose");
-const Eco = require("C:/Users/jocel/OneDrive/Documents/Jyaisseucrétaire/jojo_la_fripouille/modules/economie");
+const Eco = require("/app/modules/economie");
 
 module.exports.run = async (client, message) => {
     client.getUsers = async guild => {
