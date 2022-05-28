02b95f26e913dd45cec46570871f3d059880e5d3
Jocelyn Saint-Ellier <63356944+Jyaisseu@users.noreply.github.com>
2022-05-28 11:55:57 +0200

    Update play.js

--- .gitignore/commandes/play.js
+++ .gitignore/commandes/play.js
@@ -1,33 +1,37 @@
 const ytdl = require('ytdl-core');
-const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
+const {
+    joinVoiceChannel,
+    createAudioPlayer,
+    createAudioResource
+} = require('@discordjs/voice');
 
-module.exports.run = async (bot, message, args) => {
-    if(!message.member.voice.channel)
-        return message.channel.send("Connectez-vous au salon vocal Musique !");
-    if(message.guild.me.voice.channel)
-        return message.channel.send("Le bot est déjà en train de jouer une musique !");
-    if(!args[0])
-        return message.channel.send("Vous n'avez pas précisé de lien YouTube !");
+module.exports.run = async (Client, message, args) => {
+    if(!message.member.voice.channel) return message.channel.send('Connectez-vous à un salon vocal !');
+    if(message.guild.me.voice.channel) return message.channel.send('Le bot est déjà en train de jouer une musique !');
+    if(!args[0]) return message.channel.send("Vous n'avez pas précisé de lien YouTube !");
 
     const validate = await ytdl.validateURL(args[0]);
-    if(!validate) return message.channel.send("L'URL Youtube n'est pas valide !");
+    if(!validate) return message.channel.send("L'URL YouTube n'est pas valide !");
 
     const info = await ytdl.getInfo(args[0]);
-   const connection = joinVoiceChannel({
-       channelId: message.member.voice.channel.id,
-       guildId: message.guild.id,
-       adapterCreator: message.guild.voiceAdapterCreator,
-   });
-   const stream = ytdl(args[0], { filter: 'audioonly'});
-   const player = createAudioPlayer();
-   const resource = createAudioResource(stream);
+    const connexion = joinVoiceChannel({
+        channelId: message.member.voice.channel.id,
+        guildId: message.guild.id,
+        adapterCreator: message.guild.voiceAdapterCreator
+    });
 
-   player.play(resource);
-   connection.subscribe(player);
-   
-    message.channel.send(`Musique jouée: ${info.videoDetails.title}.`);
+    const stream = ytdl(args[0], {
+        filter: 'audioonly'
+    });
+    const player = createAudioPlayer();
+    const ressource = createAudioResource(stream);
+
+    player.play(ressource);
+    connexion.subscribe(player);
+
+    message.channel.send(`Musique jouée : ${info.videoDetails.title}.`);
 };
 
 module.exports.help = {
-    name: "play"
-}
+    name: 'play'
+};
