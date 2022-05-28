f94fb895c17a6c88bf0f553758979fca1f615a53
Jocelyn Saint-Ellier <63356944+Jyaisseu@users.noreply.github.com>
2022-05-28 11:43:10 +0200

    Update add.js

--- .gitignore/commandes/add.js
+++ .gitignore/commandes/add.js
@@ -1,95 +1,51 @@
-const Discord = require("discord.js")
-const mongoose = require("mongoose")
-const Eco = require("/app/modules/economie")
+const Eco = require('../modules/economie');
+const grade = require('../modules/grade');
 
-module.exports.run = async (client, message, args) => {
-
+module.exports.run = async (Client, message, args) => {
     if(message.author.bot) return;
-    var mentionned = message.mentions.members.first()
-    if(!message.member.permissions.has("ADMINISTRATOR")) return message.reply("Désolé, vous n'avez pas la permission d'exécuter cette commande.")
-    if(message.mentions.users.size === 0) return message.channel.send("Vous n'avez pas mentionné d'utilisateur !")
-    if(mentionned.id === message.author.id) return message.channel.send("Désolé, vous ne pouvez pas vous donner vous-même des points BG!")
-    if(args.slice(1) >0) {
-        Eco.findOne({
+    
+    let mentionned = message.mentions.members.first();
+    
+    if(!message.member.permissions.has('ADMINISTRATOR')) return message.reply("Désolé, vous n'avez pas la permission d'exécuter cette commande .");
+    
+    if(message.mentions.users.size === 0) return message.channel.send("Vous n'avez pas mentionné d'utilisateur !");
+    
+    if(mentionned.id === message.author.id) return message.channel.send('Désolé, vous ne pouvez pas vous donner à vous-même des points BG.');
+    
+    if (args.slice(1) > 0) {
+        Eco.findOne( {
             User_ID: mentionned.id
         }, (err, economie) => {
-            var somme = args.slice(1)
-            var ajout = parseInt(somme)
-            economie.xp = economie.xp + ajout
-            economie.total = economie.total + ajout   
-            message.channel.send(`Le membre ${mentionned} a bien reçu ${ajout} points BG !`)
-            var main_level = economie.level
-            var next_level = (economie.level +1) * 10                                      
-            if(next_level <= economie.xp){
-                do {
-                    var main_level = economie.level
-                    var next_level = (economie.level +1) * 10
-                    var reste = economie.xp - next_level  
-                    economie.level = economie.level +1                       
-                    economie.xp = 0 + reste
-                } while (next_level <= economie.xp)
+            let somme = args.slice(1);
+            let ajout = parseInt(somme);
+            economie.xp += ajout;
+            economie.total += ajout;
+            message.channel.send(`Le membre ${mentionned} a bien reçu ${ajout} points BG !`);
+            let main_level = economie.level;
+            let next_level = (main_level + 1) * 10;
+            
+            if(next_level <= economie.xp) {
                 
-            message.client.channels.cache.get("715144143306883092").send(`GG ${mentionned} tu viens de passer niveau ${economie.level} ! Tu deviens de plus en plus BG !`)
-
-            if((economie.level >= 2) && (economie.level<10) && (!mentionned.roles.cache.has("749245805281673248"))) {
-                const member = mentionned
-                member.roles.add('749245805281673248')
-                const débutant = message.member.guild.roles.cache.get("749245805281673248")
-                client.channels.cache.get("630431095065935884").send(`Félicitations à ${mentionned} pour avoir obtenu le grade ${débutant} ! Bienvenue dans la famille !`)
-        }else{
-            if((economie.level >= 10) && (economie.level < 20) && (!mentionned.roles.cache.has("749245861560713246"))){
-                    const member = mentionned
-                    member.roles.remove("749245805281673248")
-                    member.roles.add("749245861560713246")
-                    const actif = message.member.guild.roles.cache.get("749245861560713246")
-                    client.channels.cache.get("630431095065935884").send(`Félicitations à ${mentionned} pour avoir obtenu le grade ${actif} ! Tu es un(e) de nos membres les plus fidèles !`)
-        }else{
-            if((economie.level >= 20) && (economie.level < 50) && (!mentionned.roles.cache.has("749245970088329278"))){
-                    const member = mentionned
-                    member.roles.remove("749245861560713246")
-                    member.roles.remove("749245805281673248")
-                    member.roles.add("749245970088329278")
-                    const hyperactif = message.member.guild.roles.cache.get("749245970088329278")
-                    client.channels.cache.get("630431095065935884").send(`Félicitations à ${mentionned} pour avoir obtenu le grade ${hyperactif} ! Il ne faudrait pas que tu oublies ta vie sociale quand même !`)
-        }else{
-            if((economie.level >= 50) && (economie.level < 80) && (!mentionned.roles.cache.has("749246025197158411"))){
-                    const member = mentionned
-                    member.roles.remove("749245970088329278")
-                    member.roles.remove("749245861560713246")
-                    member.roles.remove("749245805281673248")
-                    member.roles.add("749246025197158411")
-                    const hermite = message.member.guild.roles.cache.get("749246025197158411")
-                    client.channels.cache.get("630431095065935884").send(`Mes amis, nous avons un nouveau ${hermite}... ${mentionned} a décidé de passer sa vie sur le serveur !`)
-        }else{
-            if((economie.level >= 80) && (economie.level < 100) && (!mentionned.roles.cache.has("749246081942159370"))){
-                    const member = mentionned
-                    member.roles.remove("749246025197158411")
-                    member.roles.remove("749245970088329278")
-                    member.roles.remove("749245861560713246")
-                    member.roles.remove("749245805281673248")
-                    member.roles.add("749246081942159370")
-                    const empereur = message.member.guild.roles.cache.get("749246081942159370")
-                    client.channels.cache.get("630431095065935884").send(`L'heure est grave... ${mentionned} est sacré ${empereur} de ce serveur ! Vive l'empereur !`)
-        }else{
-            if((economie.level >= 100) && (!mentionned.roles.cache.has("749246155053072405"))){
-                const member = mentionned
-                member.roles.remove("749246081942159370")
-                member.roles.rmeove("749246025197158411")
-                member.roles.remove("749245861560713246")
-                member.roles.remove("749245805281673248")
-                member.roles.remove("749245970088329278")
-                member.roles.add("749246155053072405")
-                const dieu = message.member.guild.roles.cache.get("749246155053072405")
-                client.channels.cache.get("630431095065935884").send(`Sa venue nous était annoncée... ${mentionned} s'est éveillé en temps que ${dieu} de ce serveur ! Que gloire et fierté lui soit alloué !`)
-            }
-        }}}}}
-        }
-        economie.save()})
-    }else{
-        return message.channel.send("Vous devez préciser la quantité de points BG que le membre va obtenir !");
+                do {
+                    let main_level = economie.level;
+                    let next_level = (main_level + 1) * 10;
+                    let reste = economie.xp - next_level;
+                    economie.level += 1;
+                    economie.xp = 0 = reste;
+                } while(next_level <= economie.xp);
+                
+                Client.channels.cache.find(channel => channel.name === '📈levelup').send(`GG ${mentionned} tu viens de passer niveau ${main_level} ! Tu deviens de plus en plus BG !`);
+                acquerreur = mentionned;
+                grade.run(Client, message, economie, acquerreur);
+            } economie.save();
+        });
     }
-}
+    
+    else {
+        return message.channel.send('Vous devez préciser la quantité de points BG que le membre va obtenir !');
+    };
+};
 
 module.exports.help = {
-    name: "add"
-}
+    name: 'add'
+};
