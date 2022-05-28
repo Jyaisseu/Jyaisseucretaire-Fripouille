f857fdefafeab3682d256d68644b54b8c1962052
Jocelyn Saint-Ellier <63356944+Jyaisseu@users.noreply.github.com>
2022-05-28 11:57:09 +0200

    Update top5.js

--- .gitignore/commandes/top5.js
+++ .gitignore/commandes/top5.js
@@ -1,25 +1,29 @@
-const Discord = require("discord.js");
-const { MessageEmbed } = require("discord.js");
-const mongoose = require("mongoose");
-const Eco = require("/app/modules/economie");
+const {
+    MessageEmbed
+} = require('discord.js');
+const Eco = require('../modules/economie');
 
-module.exports.run = async (client, message) => {
-    client.getUsers = async guild => {
-        const data = await Eco.find({ __v: 0 });
+module.exports.run = async (Client, message) => {
+    Client.getUsers = async guild => {
+        const data = await Eco.find({
+            __v: 0
+        });
         if (data) return data;
         else return;
     };
     const topembed = new MessageEmbed()
-    .setTitle("Top 5 des plus BGs du serveur")
-    .setDescription("Mais en vrai vous êtes tous un peu BG ici")
-    await client.getUsers(message.guild).then(p => {
-        p.sort((a, b) => (a.total < b.total) ? 1 : -1).splice(0, 5).forEach(e => {
+    .setTitle('Top 5 des plus gros BG du serveur')
+    .setDescription('Mais en vrai vous êtes tous un peu BG ici')
+    await Client.getUsers(message.guild).then(p => {
+        p.sort((a, b) => (a.total < b.total) ? 1: -1).splice(0, 5).forEach(e => {
             topembed.addField(e.pseudo, `BG niveau ${e.level} avec ${e.xp} points BG`);
         });
     });
-    message.channel.send({embeds: [topembed]});
-}
+    message.channel.send({
+        embeds: [topembed]
+    });
+};
 
 module.exports.help = {
-    name: "top5"
-}
+    name: 'top5'
+};
