e7abd5ca0bcf9f7dfd8a73670b1d038494fdefd8
Jocelyn Saint-Ellier <63356944+Jyaisseu@users.noreply.github.com>
2022-05-28 11:56:42 +0200

    Update stop.js

--- .gitignore/commandes/stop.js
+++ .gitignore/commandes/stop.js
@@ -1,23 +1,21 @@
-const Discord = require("discord.js");
-const ytdl = require('ytdl-core');
-const {joinVoiceChannel } = require('@discordjs/voice')
+const {
+    joinVoiceChannel,
+} = require('@discordjs/voice');
 
-module.exports.run = async (client, message) => {
-    if(!message.member.voice.channel)
-        return message.channel.send("Connectez-vous au salon vocal Musique !")
-    if(!message.guild.me.voice.channel)
-        return message.channel.send("Le bot ne joue pas de musiques actuellement !")
-    if(message.guild.me.voice.channel.id !== message.member.voice.channel.id)
-        return message.channel.send("Vous devez être connecté dans le même salon vocal que le bot !")
-    const connection = joinVoiceChannel({
-        channelId: message.member.voice.channel.id,
+module.exports.run = async(Client, message) => {
+    if(!message.member.voice.channel) return message.channel.send('Connectez vous à un salon vocal !');
+    if(!message.guild.me.voice.channel) return message.channel.send('Le bot ne joue actuellement pas de musique !');
+    if(message.guild.me.voice.channel.id !== message.member.voice.channel.id) return message.channel.send('Vous devez être connecté dans le même salon vocal que le bot !');
+
+    const connexion = joinVoiceChannel({
+        channelId: message.member.voice.channel.id, 
         guildId: message.guild.id,
-        adapterCreator: message.guild.voiceAdapterCreator,
+        adapterCreator: message.guild.voiceAdapterCreator
     });
-    connection.destroy()
-    message.channel.send("La musique a correctement été arrêtée !");
-}
+    connexion.destroy();
+    message.channel.send('La musique a correctement été arrêtée !');
+};
 
 module.exports.help = {
-    name: "stop"
-}
+    name: 'stop'
+};
