00eee4b4341b1f76dbedd1a34925ff7d85a169c5
Jocelyn Saint-Ellier <63356944+Jyaisseu@users.noreply.github.com>
2022-06-01 17:39:38 +0200

    Update level.js

--- .gitignore/commandes/level.js
+++ .gitignore/commandes/level.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const Eco = require('../modules/economie');
 
-module.exports.run = async (Client, message) => {
+module.exports.run = async (Client, interaction) => {
     Eco.findOne({
-        User_ID: message.author.id
+        User_ID: interaction.member.id
     }, (err, economie) => {
         let main_level = economie.level;
-        message.channel.send(`Tu es actuellement BG niveau ${main_level} avec en supplément ${economie.xp +1} points BG, donc il te manque ${(main_level + 1) * 10 - economie.xp -1} points pour passer au niveau suivant.`);
+        interaction.reply(`Tu es actuellement BG niveau ${main_level} avec en supplément ${economie.xp +1} points BG, donc il te manque ${(main_level + 1) * 10 - economie.xp -1} points pour passer au niveau suivant.`);
     });
 };
 
