ed02a90991f713bf571d08c85000d1a27b74de15
Jocelyn Saint-Ellier <63356944+Jyaisseu@users.noreply.github.com>
2022-06-01 17:37:23 +0200

    Update coucou.js

--- .gitignore/commandes/coucou.js
+++ .gitignore/commandes/coucou.js
@@ -1,5 +1,5 @@
-module.exports.run = async (Client, message) => {
-    message.channel.send(Èt bah coucou à toi aussi, ${message.author.username} !`);
+module.exports.run = async (Client, interaction) => {
+    interaction.reply(`Et bah coucou à toi aussi, ${interaction.user.username} !`);
 };
 
 module.exports.help = {
