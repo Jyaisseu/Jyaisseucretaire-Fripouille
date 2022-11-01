f2f0566d55d8ee3800c7dbd0b01b761d2241be8c
Jocelyn Saint-Ellier <63356944+Jyaisseu@users.noreply.github.com>
2022-11-01 22:42:25 +0100

    Update degrade.js

--- .gitignore/modules/degrade.js
+++ .gitignore/modules/degrade.js
@@ -1,4 +1,4 @@
-module.exports.run = async (Client, interaction, economie, perdant) => {
+module.exports.run = async (client, interaction, economie, perdant) => {
     debutant = interaction.guild.roles.cache.find(role => role.name === 'Débutant');
     actif = interaction.guild.roles.cache.find(role => role.name === 'Actif');
     hyperactif = interaction.guild.roles.cache.find(role => role.name === 'Hyperactif');
@@ -8,27 +8,37 @@ module.exports.run = async (Client, interaction, economie, perdant) => {
 
     if(economie.level < 2) {
         perdant.roles.remove(debutant, actif, hyperactif, hermite, empereur, dieu);
-        Client.channels.cache.find(channel => channel.name === '💬général').send(`${perdant} a perdu tous ses grades !`);
-    }else if((economie.level >= 2) && (economie.level < 10) && (!perdant.roles.cache.some(debutant))) {
-        perdant.roles.remove(actif, hyperactif, hermite, empereur, dieu);
+        client.channels.cache.find(channel => channel.name === '💬général').send(`${perdant} a perdu tous ses grades !`);
+    }else if((economie.level >= 2) && (economie.level < 10) && (!perdant.roles.cache.some(role => role.name === 'debutant'))) {
+        perdant.roles.remove(actif);
+        perdant.roles.remove(hyperactif);
+        perdant.roles.remove(hermite);
+        perdant.roles.remove(empereur);
+        perdant.roles.remove(dieu);
         perdant.roles.add(debutant);
-        Client.channels.cache.find(channel => channel.name === '💬général').send(`${perdant} redescend au grade ${débutant} !`);
+        client.channels.cache.find(channel => channel.name === '💬général').send(`${perdant} redescend au grade ${débutant} !`);
     }else if((economie.level >= 10) && (economie.level < 20) && (!perdant.roles.cache.some(role => role.name === 'Actif'))) {
-        perdant.roles.remove(hyperactif, hermite, empereur, dieu);
+        perdant.roles.remove(hyperactif);
+        perdant.roles.remove(hermite);
+        perdant.roles.remove(empereur);
+        perdant.roles.remove(dieu);
         perdant.roles.add(actif);
-        Client.channels.cache.find(channel => channel.name === '💬général').send(`${perdant} redescend au grade ${actif} !`);
-    }else if((economie.level >= 20) && (economie.level < 50) && (!perdant.roles.cache.some(hyperactif))) {
-        perdant.roles.remove(hermite, empereur, dieu);
+        client.channels.cache.find(channel => channel.name === '💬général').send(`${perdant} redescend au grade ${actif} !`);
+    }else if((economie.level >= 20) && (economie.level < 50) && (!perdant.roles.cache.some(role => role.name === 'Hyperactif'))) {
+        perdant.roles.remove(hermite);
+        perdant.roles.remove(empereur);
+        perdant.roles.remove(dieu);
         perdant.roles.add(hyperactif);
-        Client.channels.cache.find(channel => channel.name === '💬général').send(`${perdant} redescend au grade ${hyperactif} !`);
-    }else if((economie.level >= 50) && (economie.level < 80) && (!perdant.roles.cache.some(hermite))) {
-        perdant.roles.remove(empereur, dieu);
+        client.channels.cache.find(channel => channel.name === '💬général').send(`${perdant} redescend au grade ${hyperactif} !`);
+    }else if((economie.level >= 50) && (economie.level < 80) && (!perdant.roles.cache.some(role => role.name === 'hermite'))) {
+        perdant.roles.remove(empereur);
+        perdant.roles.remove(dieu);
         perdant.roles.add(hermite);
-        Client.channels.cache.find(channel => channel.name === '💬général').send(`${perdant} redescend au grade ${hermite} !`);
-    }else if((economie.level >= 80) && (economie.level < 100) && (!perdant.roles.cache.some(empereur))) {
+        client.channels.cache.find(channel => channel.name === '💬général').send(`${perdant} redescend au grade ${hermite} !`);
+    }else if((economie.level >= 80) && (economie.level < 100) && (!perdant.roles.cache.some(role => role.name === 'empereur'))) {
         perdant.roles.remove(dieu);
         perdant.roles.add(empereur);
-        Client.channels.cache.find(channel => channel.name === '💬général').send(`${perdant} redescend au grade ${empereur} !`);
+        client.channels.cache.find(channel => channel.name === '💬général').send(`${perdant} redescend au grade ${empereur} !`);
     }
 };
 
