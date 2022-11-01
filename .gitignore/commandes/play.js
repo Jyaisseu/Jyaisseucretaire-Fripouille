87615cb3f6125afefe6e2994c78b010e102d6e4d
Jocelyn Saint-Ellier <63356944+Jyaisseu@users.noreply.github.com>
2022-11-01 22:40:06 +0100

    Update play.js

--- .gitignore/commandes/play.js
+++ .gitignore/commandes/play.js
@@ -5,14 +5,16 @@ const {
     createAudioResource
 } = require('@discordjs/voice');
 
-module.exports.run = async (Client, interaction) => {
+module.exports.run = async (client, interaction) => {
     if(!interaction.member.voice.channel) return interaction.reply('Connectez-vous à un salon vocal !');
-    if(interaction.guild.me.voice.channel) return interaction.reply('Le bot est déjà en train de jouer une musique !');
-
+    
     const validate = await ytdl.validateURL(interaction.options.getString('lien'));
     if(!validate) return interaction.reply("L'URL YouTube n'est pas valide !");
+    interaction.reply('Recherche de la musique en cours...');
 
     const info = await ytdl.getInfo(interaction.options.getString('lien'));
+    interaction.editReply(`Musique jouée : ${info.videoDetails.title}.`);
+
     const connexion = joinVoiceChannel({
         channelId: interaction.member.voice.channel.id,
         guildId: interaction.guild.id,
@@ -20,15 +22,15 @@ module.exports.run = async (Client, interaction) => {
     });
 
     const stream = ytdl(interaction.options.getString('lien'), {
-        filter: 'audioonly'
+        filter: 'audioonly',
+        quality: 'highestaudio',
+        highWaterMark: 1 << 25
     });
     const player = createAudioPlayer();
     const ressource = createAudioResource(stream);
 
     player.play(ressource);
     connexion.subscribe(player);
-
-    interaction.reply(`Musique jouée : ${info.videoDetails.title}.`);
 };
 
 module.exports.help = {
