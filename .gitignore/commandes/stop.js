47bdcf17c023a0a705981d31fb98c64f9e632702
Jocelyn Saint-Ellier <63356944+Jyaisseu@users.noreply.github.com>
2022-11-01 22:40:54 +0100

    Update stop.js

--- .gitignore/commandes/stop.js
+++ .gitignore/commandes/stop.js
@@ -2,11 +2,8 @@ const {
     joinVoiceChannel,
 } = require('@discordjs/voice');
 
-module.exports.run = async(Client, interaction) => {
+module.exports.run = async(client, interaction) => {
     if(!interaction.member.voice.channel) return interaction.reply('Connectez vous à un salon vocal !');
-    if(!interaction.guild.me.voice.channel) return interaction.reply('Le bot ne joue actuellement pas de musique !');
-    if(interaction.guild.me.voice.channel.id !== interaction.member.voice.channel.id) return interaction.reply('Vous devez être connecté dans le même salon vocal que le bot !');
-
     const connexion = joinVoiceChannel({
         channelId: interaction.member.voice.channel.id, 
         guildId: interaction.guild.id,
