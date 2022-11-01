067e0005ea9703982368a63f1814aefb2d4d6f30
Jocelyn Saint-Ellier <63356944+Jyaisseu@users.noreply.github.com>
2022-11-01 22:41:37 +0100

    Update grade.js

--- .gitignore/modules/grade.js
+++ .gitignore/modules/grade.js
@@ -1,4 +1,4 @@
-module.exports.run = async (Client, interaction, economie, acquerreur) => {
+module.exports.run = async (client, interaction, economie, acquerreur) => {
     const member = acquerreur
     const debutant = interaction.guild.roles.cache.find(role => role.name === 'Débutant');
     const actif = interaction.guild.roles.cache.find(role => role.name === 'Actif');
@@ -9,29 +9,29 @@ module.exports.run = async (Client, interaction, economie, acquerreur) => {
 
     if(economie.level === 2){
         member.roles.add(debutant);
-        Client.channels.cache.find(channel => channel.name === '💬général').send(`Félicitations à ${acquerreur} pour avoir obtenu le grade ${debutant} ! Bienvenue dans le club !`);
+        client.channels.cache.find(channel => channel.name === '💬général').send(`Félicitations à ${acquerreur} pour avoir obtenu le grade ${debutant} ! Bienvenue dans le club !`);
     }else if(economie.level === 10){
         member.roles.remove(debutant);
-        member.roles.add
-        Client.channels.cache.find(channel => channel.name === '💬général').send(`Félicitations à ${acquerreur} pour avoir obtenu le grae ${actif} ! Tu es un de nos membres les plus fidèles !`);
+        member.roles.add(actif);
+        client.channels.cache.find(channel => channel.name === '💬général').send(`Félicitations à ${acquerreur} pour avoir obtenu le grae ${actif} ! Tu es un de nos membres les plus fidèles !`);
     }else if(economie.level === 20){
         member.roles.remove(debutant);
         member.roles.remove(actif);
         member.roles.add(hyperactif);
-        Client.channels.cache.find(channel => channel.name === '💬général').send(`Félicitations à ${acquerreur} pour avoir obtenu le grade ${hyperactif} ! Il ne faudrait pas que tu oublies ta vie sociale quand même !`);
+        client.channels.cache.find(channel => channel.name === '💬général').send(`Félicitations à ${acquerreur} pour avoir obtenu le grade ${hyperactif} ! Il ne faudrait pas que tu oublies ta vie sociale quand même !`);
     }else if(economie.level === 50){
         member.roles.remove(debutant);
         member.roles.remove(actif);
         member.roles.remove(hyperactif);
         member.roles.add(hermite);
-        Client.channels.cache.find(channel => channel.name === '💬général').send(`Mes amis, nous avons un nouveau ${hermite}... ${acquerreur} a décidé de passer sa vie sur le serveur !`);
+        client.channels.cache.find(channel => channel.name === '💬général').send(`Mes amis, nous avons un nouveau ${hermite}... ${acquerreur} a décidé de passer sa vie sur le serveur !`);
     }else if(economie.level === 80){
         member.roles.remove(debutant);
         member.roles.remove(actif);
         member.roles.remove(hyperactif);
         member.roles.remove(hermite);
         member.roles.add(empereur);
-        Client.channels.cache.find(channel => channel.name === '💬général').send(`L'heure est grave... ${acquerreur} est sacré ${empereur} de ce serveur ! Gloire à lui !`);
+        client.channels.cache.find(channel => channel.name === '💬général').send(`L'heure est grave... ${acquerreur} est sacré ${empereur} de ce serveur ! Gloire à lui !`);
     }else if(economie.level === 100){
         member.roles.remove(debutant);
         member.roles.remove(actif);
@@ -39,7 +39,7 @@ module.exports.run = async (Client, interaction, economie, acquerreur) => {
         member.roles.remove(hermite);
         member.roles.remove(empereur);
         member.roles.add(dieu);
-        Client.channels.cache.find(channel => channel.name === '💬général').send(`Sa venue nous était annoncée... ${acquerreur} s'éveille en tant que dieu tout puissant sur le serveur ! Que gloire et fierté lui soit alloué !`);
+        client.channels.cache.find(channel => channel.name === '💬général').send(`Sa venue nous était annoncée... ${acquerreur} s'éveille en tant que dieu tout puissant sur le serveur ! Que gloire et fierté lui soit alloué !`);
     };
 };
 
